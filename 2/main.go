710d2cc9ee183b6c1537dda0b1e21e63a2002634
Josh Whetton <josh@whetton.net>
2022-12-03 13:43:35 -0500

    Helpers in place
    Wrote the helpers to simplify the loop.

--- 2/main.go
+++ 2/main.go
@@ -1,6 +1,85 @@
 package main
 
-const (
-	ROCK, PAPER, SCISSORS int = 1, 2, 3 // Shape values
-	LOSE, DRAW, WIN       int = 0, 3, 6 // Result values
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
 )
+
+type shape int
+type result int
+
+const (
+	inputFile             string = "input"
+	ROCK, PAPER, SCISSORS shape  = 1, 2, 3 // Shape values
+	LOSE, DRAW, WIN       result = 0, 3, 6 // Result values
+)
+
+func parseShapes(rawShapes []string) ([]shape, error) {
+	shapes := make([]shape, 0)
+	for _, rs := range rawShapes {
+		parsed, err := parseShape(rs)
+		if err != nil {
+			return shapes, err
+		}
+		shapes = append(shapes, parsed)
+	}
+	return shapes, nil
+}
+
+func parseShape(rawShape string) (shape, error) {
+	var parsedShape shape
+	switch strings.ToUpper(rawShape) {
+	case "A", "X":
+		parsedShape = ROCK
+	case "B", "Y":
+		parsedShape = PAPER
+	case "C", "Z":
+		parsedShape = SCISSORS
+	default:
+		return 0, errors.New(fmt.Sprintf("Invalid shape: %s", rawShape))
+	}
+	return parsedShape, nil
+}
+
+func duel(theirShape, myShape shape) result {
+	switch {
+	case theirShape == myShape:
+		return DRAW
+	case myShape == ROCK && theirShape == SCISSORS:
+		return WIN
+	case myShape == PAPER && theirShape == ROCK:
+		return WIN
+	case myShape == SCISSORS && theirShape == PAPER:
+		return WIN
+	default:
+		return LOSE
+	}
+}
+
+func main() {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer file.Close()
+
+	// for each line in input file...
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// parse the shapes in the study guide
+		parsedShapes, err := parseShapes(strings.Split(scanner.Text(), " "))
+		if err != nil {
+			fmt.Println(err)
+		}
+		theirShape := parsedShapes[0]
+		myShape := parsedShapes[1]
+		fmt.Printf("Their shape: %d, My shape: %d\n", theirShape, myShape)
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Println(err)
+	}
+}
