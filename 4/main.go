da67052e7ca93ee245737a42ff07718b93b04852
Josh Whetton <whetton.josh@gmail.com>
2022-12-14 09:33:51 -0500

    Day4 part 1 complete
    Decided to use fmt.Fscanf instead of bufio.Scanner to
    loop through and process the file.

--- 4/main.go
+++ 4/main.go
@@ -1,14 +1,42 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
+	"io"
 	"os"
 )
 
 const (
 	inputFile = "input"
 )
+
+// Duty contains a range of work duty. Mostly an
+// initialization shortcut and isn't strictly
+// necessary.
+type Duty struct {
+	Start, Stop int
+}
+
+// A Pair represents Duty assigned to a pair of elves.
+type Pair struct {
+	First, Second Duty
+}
+
+// RedundantWorkDuty will return true when either Duty
+// fully contians the other.
+func RedundantWorkDuty(p Pair) bool {
+	// There's surely a better way to do this.
+	switch {
+	case p.First.Start <= p.Second.Start && p.First.Stop >= p.Second.Stop:
+		// p.First fully contains p.Second Duty
+		return true
+	case p.First.Start >= p.Second.Start && p.First.Stop <= p.Second.Stop:
+		// p.Second fully contains p.First Duty
+		return true
+	default:
+		return false
+	}
+}
 
 func main() {
 	file, err := os.Open(inputFile)
@@ -17,12 +45,17 @@ func main() {
 	}
 	defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		// line = scanner.Text()
-	}
+	var counter int
+	var first, second Duty
+	for {
+		if _, err := fmt.Fscanf(file, "%d-%d,%d-%d\n", &first.Start, &first.Stop, &second.Start, &second.Stop); err == io.EOF {
+			// nothing more to process; end of file.
+			break
+		}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Printf("Scanner error: %v", err)
+		if RedundantWorkDuty(Pair{first, second}) {
+			counter++
+		}
 	}
+	fmt.Printf("Count of fully redundant work duties: %d\n", counter)
 }
