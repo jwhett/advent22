837946e426445b7d4d5510152fa5c2d4946aa58b
Josh Whetton <whetton.josh@gmail.com>
2022-12-20 19:54:54 -0500

    Refactor out helpers from day 5

--- 5/dayfive/dayfive_test.go
+++ 5/dayfive/dayfive_test.go
@@ -42,24 +42,6 @@ func PrepareTestInput() io.Reader {
 	return strings.NewReader(testInput)
 }
 
-func TestPop(t *testing.T) {
-	if first, rest := Pop(helperSlice); first != 1 || rest[0] != 2 {
-		t.Errorf("Pop failed. Expected first: 1, got %d. Expected rest: [2 3 4 5], got %v\n", first, rest)
-	}
-}
-
-func TestLast(t *testing.T) {
-	if init, last := Last(helperSlice); init[0] != 1 || last != 5 {
-		t.Errorf("Pop failed. Expected init: [1 2 3 4], got %v. Expected last: 5, got %d\n", init, last)
-	}
-}
-
-func TestTakeN(t *testing.T) {
-	if remaining, taken := TakeN(2, helperSlice); remaining[0] != 1 || taken[0] != 4 {
-		t.Errorf("Pop failed. Expected remaining: [1 2 3], got %v. Expected taken: [4 5], got %v\n", remaining, taken)
-	}
-}
-
 func TestParseMap(t *testing.T) {
 	stacks := inputReader.ParseMap(mapDimensions)
 	// Ensure we receive the proper number of stacks from
