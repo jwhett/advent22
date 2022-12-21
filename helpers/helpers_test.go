bf4fd4b83146e6aeffd662a1db1ef916cdc164d2
Josh Whetton <whetton.josh@gmail.com>
2022-12-20 20:40:29 -0500

    Add generic slice comparison function

--- helpers/helpers_test.go
+++ helpers/helpers_test.go
@@ -25,3 +25,21 @@ func TestTakeN(t *testing.T) {
 		t.Errorf("Pop failed. Expected remaining: [1 2 3], got %v. Expected taken: [4 5], got %v\n", remaining, taken)
 	}
 }
+
+func TestSliceEqual(t *testing.T) {
+	// Setup our vars two ways. First from
+	// a []int literal, second by appending
+	// elements to an initially empty slice.
+	first := []int{1, 2, 3, 4, 5}
+	second := make([]int, 0)
+	for i := 1; i < 6; i++ {
+		second = append(second, i)
+	}
+
+	firstString, secondString := []string{"one", "two"}, []string{"one", "two"}
+	firstFloat, secondFloat := []float32{1.2, 3.4, 5.6}, []float32{1.2, 3.4, 5.6}
+	if !SliceEqual(first, second) || !SliceEqual(firstString, secondString) || !SliceEqual(firstFloat, secondFloat) {
+		t.Error("Equal slices failed to match.")
+	}
+
+}
