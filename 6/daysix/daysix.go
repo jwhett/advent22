2d7691c18038b700229a0a1d843448a0de0873d3
Josh Whetton <whetton.josh@gmail.com>
2022-12-21 22:07:32 -0500

    Add more tests!
    No implementation yet. Just thinking about behaviors.

--- 6/daysix/daysix.go
+++ 6/daysix/daysix.go
@@ -1,5 +1,12 @@
 package daysix
 
+func Unique(in string) bool {
+	// TODO unemplemented
+	// sort and check for successive repeats
+	return true
+}
+
 func IdentifyTransmissionBit(freq string) (marker string, count int) {
+	// TODO unemplemented
 	return "", 0
 }
--- 6/daysix/daysix_test.go
+++ 6/daysix/daysix_test.go
@@ -2,23 +2,43 @@ package daysix
 
 import "testing"
 
-type FreqTest struct {
-	Freq           string
-	ExpectedMarker string
-	ExpectedCount  int
+func TestUnique(t *testing.T) {
+	tests := []struct {
+		Input    string
+		Expected bool
+	}{
+		{Input: "aabc", Expected: false},
+		{Input: "abad", Expected: false},
+		{Input: "", Expected: false},
+		{Input: "a", Expected: true},
+		{Input: "ca", Expected: true},
+		{Input: "fedc", Expected: true},
+	}
+
+	for _, test := range tests {
+		if Unique(test.Input) != test.Expected {
+			t.Errorf("Unique failed. %q was expected to be %t\n", test.Input, test.Expected)
+		}
+	}
 }
 
 func TestTransmissionBitIdentification(t *testing.T) {
-	tests := []FreqTest{
-		{"bvwbjplbgvbhsrlpgdmjqwftvncz", "vwbj", 5},
-		{"nppdvjthqldpwncqszvftbrmjlhg", "pdvj", 6},
-		{"nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", "fntj", 10},
-		{"zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", "qfrl", 11},
+	tests := []struct {
+		Freq           string
+		ExpectedMarker string
+		ExpectedCount  int
+	}{
+		{Freq: "", ExpectedMarker: "", ExpectedCount: -1},
+		{Freq: "aabbccddeeffgghhiijjkkllmmnn", ExpectedMarker: "", ExpectedCount: -1},
+		{Freq: "bvwbjplbgvbhsrlpgdmjqwftvncz", ExpectedMarker: "vwbj", ExpectedCount: 5},
+		{Freq: "nppdvjthqldpwncqszvftbrmjlhg", ExpectedMarker: "pdvj", ExpectedCount: 6},
+		{Freq: "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg", ExpectedMarker: "fntj", ExpectedCount: 10},
+		{Freq: "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw", ExpectedMarker: "qfrl", ExpectedCount: 11},
 	}
 
 	for _, test := range tests {
 		if marker, count := IdentifyTransmissionBit(test.Freq); count != test.ExpectedCount || marker != test.ExpectedMarker {
-			t.Errorf("Failed to identify transmission bit. Got: marker=> %s count=> %d, Wanted marker=> %s count => %d\n", marker, count, test.ExpectedMarker, test.ExpectedCount)
+			t.Errorf("Failed to identify transmission bit.\nGot: marker=> %s count => %d\nWanted: marker=> %s count => %d\n", marker, count, test.ExpectedMarker, test.ExpectedCount)
 		}
 	}
 }
