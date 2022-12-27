dd70645971e2dd7340f406e739f2eba54b1f531e
Josh Whetton <whetton.josh@gmail.com>
2022-12-26 20:56:52 -0500

    Solved day 6 part 1

--- 6/main.go
+++ 6/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+
+	df "github.com/jwhett/advent22/6/daysix"
 )
 
 const (
@@ -17,9 +19,13 @@ func main() {
 	}
 	defer file.Close()
 
+	var line string
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		// line = scanner.Text()
+		line = scanner.Text()
+		if result := df.IdentifyTransmissionBit(line); result.Index > 0 {
+			fmt.Printf("Found marker %q after processing %d characters.\n", result.Marker, result.Index)
+		}
 	}
 
 	if err := scanner.Err(); err != nil {
